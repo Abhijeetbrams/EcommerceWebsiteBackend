02449bfd7a08c669c60805c1b9593a3f91163d08
Abhijeet Brahmchari <abybrams@gmail.com>
2025-01-16 16:34:57 +0530

    Cardinality+Fetch Types+Fix Of Infinite Recurssion During Serialization

--- src/main/java/com/ecommerce/productservice/model/Category.java
+++ src/main/java/com/ecommerce/productservice/model/Category.java
@@ -1,12 +1,21 @@
 package com.ecommerce.productservice.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity(name="categories")
 public class Category extends BaseModel{
     private String value;
+
+    @OneToMany(mappedBy = "category") // this basically said that the category field in the Product class is the owner of the relationship
+            // and don't create a new column in the category table to store the relationship
+    List<Product> products; // This is not mandatory, but it is a good practice to have a list of products in the category
+    // if you want to fetch all the products for a particular category
 }
--- src/main/java/com/ecommerce/productservice/model/Product.java
+++ src/main/java/com/ecommerce/productservice/model/Product.java
@@ -1,5 +1,7 @@
 package com.ecommerce.productservice.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
@@ -12,6 +14,7 @@ public class Product extends BaseModel{
     private String title;
     private Double price;
     @ManyToOne
+    @JsonIgnoreProperties("products") // Ignore the `products` field in the `Category` during serialization otherwise infinite recursion will occur.
     private Category category;
     private String description;
     private String image;
