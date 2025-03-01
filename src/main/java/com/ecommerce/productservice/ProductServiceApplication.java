26e3cb93ff88756f822a38893a6f98a10a43076b
Abhijeet Brahmchari <abybrams@gmail.com>
2025-03-02 02:29:14 +0530

    Some Random Changes

--- src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
+++ src/main/java/com/ecommerce/productservice/ProductServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication // This annotation is used to enable the auto-configuration feature of the Spring Boot application.
 public class ProductServiceApplication {
 
+    // This is the main method that is used to run the Spring Boot application.
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
